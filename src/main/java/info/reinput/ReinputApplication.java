158a7efbe963f4dc007175f298b7ad9b3d583d62
Jhsysng <ysng1118@naver.com>
2024-09-21 17:17:13 +0900

    initial commit

---
+++ src/main/java/info/reinput/ReinputApplication.java
@@ -0,0 +1,13 @@
+package info.reinput;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReinputApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReinputApplication.class, args);
+    }
+
+}
---
+++ src/test/java/info/reinput/ReinputApplicationTests.java
@@ -0,0 +1,13 @@
+package info.reinput;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReinputApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
