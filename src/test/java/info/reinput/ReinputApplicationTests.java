710f1c95a9bafa3fef472093f0b856ff8d55213a
Jhsysng <ysng1118@naver.com>
2024-09-23 10:27:03 +0900

    chore: querydsl, aws

--- src/test/java/info/reinput/ReinputApplicationTests.java
+++ src/test/java/info/reinput/ReinputApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class ReinputApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
+	@Test
+	void contextLoads() {
+	}
 
 }
