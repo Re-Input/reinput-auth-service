987f608798d04705dd53501c824a9f10581bf9b4
Jhsysng <ysng1118@naver.com>
2024-09-25 14:38:00 +0900

    feat: insights domain model

---
+++ src/main/java/info/reinput/insight/domain/InsightDetail.java
@@ -0,0 +1,20 @@
+package info.reinput.insight.domain;
+
+import jakarta.persistence.Embeddable;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import java.time.LocalDateTime;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Embeddable
+public class InsightDetail {
+    private String url;
+    private String memo;
+    private Long viewCount;
+    @LastModifiedDate
+    private LocalDateTime lastViewedAt;
+}
