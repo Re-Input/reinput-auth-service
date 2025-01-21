187d8cecd0948a36030c5828872dedd0f7380178
Jhsysng <ysng1118@naver.com>
2025-01-21 21:11:33 +0900

    RE-166 chore: feign 의존성 추가 및 configuration 설정

---
+++ src/main/java/info/reinput/global/config/FeignConfig.java
@@ -0,0 +1,15 @@
+package info.reinput.global.config;
+
+import feign.Logger;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableFeignClients(basePackages = "info.reinput.member.infra.client")
+public class FeignConfig {
+    @Bean
+    Logger.Level feignLoggerLevel() {
+        return Logger.Level.FULL;
+    }
+}
