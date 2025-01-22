aca2a681113c9926eeb48c02617f254c840af65e
Jhsysng <ysng1118@naver.com>
2025-01-22 14:04:16 +0900

    RE-167 feat: jwt 인증 스키마 설정

--- src/main/java/info/reinput/global/config/SwaggerConfig.java
+++ src/main/java/info/reinput/global/config/SwaggerConfig.java
@@ -25,16 +25,31 @@ public class SwaggerConfig {
                                 )
                         )
                 );
+        SecurityScheme jwtAuth = new SecurityScheme()
+                .type(SecurityScheme.Type.HTTP)
+                .scheme("bearer")
+                .bearerFormat("JWT")
+                .in(SecurityScheme.In.HEADER)
+                .name("Authorization");
 
         SecurityRequirement securityRequirement = new SecurityRequirement()
-                .addList("kakao_oauth2", "account_email");
+                .addList("kakao_oauth2", "account_email")
+                .addList("jwt_auth");
         Info info = new Info()
                 .title("Reinput Member")
                 .version("v0.0.1")
-                .description("Reinput Member Service (auth service)");
+                .description("""
+                    Reinput Member Service (auth service)
+                    
+                    인증 플로우:
+                    1. 카카오 OAuth 로그인 수행
+                    2. 성공 시 JWT 토큰 발급
+                    3. 발급받은 JWT를 Authorization 헤더에 Bearer 형식으로 포함하여 API 호출
+                    """);
         return new OpenAPI()
                 .components(new Components()
-                        .addSecuritySchemes("kakao_oauth2", kakaoOauth))
+                        .addSecuritySchemes("kakao_oauth2", kakaoOauth)
+                        .addSecuritySchemes("jwt_auth", jwtAuth))
                 .security(List.of(securityRequirement))
                 .info(info);
     }
